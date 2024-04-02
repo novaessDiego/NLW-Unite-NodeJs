2eb0dfbea0314d894a0357584b161d8bd212ae55
Diego <novaessdiego@gmail.com>
2024-04-02 18:45:57 -0300

    feat: aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import fastify from 'fastify'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+import { createEvent } from './routes/crate-event';
+import { registerForEvent } from './routes/register-for-event';
+import { getEvent } from './routes/get-event';
+import { getAttendeeBadge } from './routes/get-attendee-badge';
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello NLW Unite'
-})
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Running!')
